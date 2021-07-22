b82e42191d43597d5634c143b65aeb74e0dbe30f
AlexBanning <banninghg80@gmail.com>
2021-07-21 22:39:13 -0500

    remove old test and make mocha script recursive

---
+++ test/models/Book.test.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+var Book = require('../../models/Book');
+
+describe ('book', function() {
+    it('should be invalid if title is empty', function(done) {
+        var b = new Book();
+        
+        b.validate(function(err) {
+            expect(err.errors.title).to.exist;
+            done();
+        });
+    });
+});
